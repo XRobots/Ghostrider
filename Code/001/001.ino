450847e41904e15b04c90561f4a811d452713da2
James Bruton <xrobots@gmail.com>
2025-01-19 18:50:53 +0000

    Add files via upload

---
+++ Code/001/001.ino
@@ -0,0 +1,26 @@
+#include <Servo.h>
+Servo myservo;
+int pot;
+int output;
+
+void setup() {
+
+  pinMode(A0,INPUT);
+  Serial.begin(115200);
+  myservo.attach(9);
+  myservo.writeMicroseconds(1500);
+
+}
+
+void loop() {
+  pot = analogRead(A0);
+  output = map(pot, 0,1023,1500,2000);
+  myservo.writeMicroseconds(output);
+  Serial.println(output);
+  
+  
+
+  delay(10);
+
+
+}
